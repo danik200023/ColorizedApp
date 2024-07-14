a4860574d46d48a4a58f69c16c6e437a44a7f1b5
danila <danik200023@proton.me>
2024-07-14 23:43:30 +0400

    Work with logic

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var coloredView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLabel.text = getValue(from: redSlider)
+        greenLabel.text = getValue(from: greenSlider)
+        blueLabel.text = getValue(from: blueSlider)
+        
+        coloredView.layer.cornerRadius = 15
+        setColor()
     }
 
-
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redSlider:
+            redLabel.text = getValue(from: redSlider)
+        case greenSlider:
+            greenLabel.text = getValue(from: greenSlider)
+        default:
+            blueLabel.text = getValue(from: blueSlider)
+        }
+    }
+    
+    private func setColor() {
+        coloredView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue:  CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func getValue(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
 
