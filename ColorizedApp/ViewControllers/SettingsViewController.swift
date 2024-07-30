caa5b649cc7140572817ff774f988d012e4c64be
danila <danik200023@proton.me>
2024-07-31 01:18:45 +0400

    Add text fields and logic

--- ColorizedApp/ViewControllers/SettingsViewController.swift
+++ ColorizedApp/ViewControllers/SettingsViewController.swift
@@ -17,6 +17,10 @@ final class SettingsViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
     @IBOutlet var coloredView: UIView!
     
     var mainVCBgColor: UIColor!
@@ -26,19 +30,28 @@ final class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate = self
+        
         let colorComponents = getRGB(from: mainVCBgColor)
         redSlider.value = Float(colorComponents.red)
         greenSlider.value = Float(colorComponents.green)
         blueSlider.value = Float(colorComponents.blue)
         
-        redLabel.text = getValue(from: redSlider)
-        greenLabel.text = getValue(from: greenSlider)
-        blueLabel.text = getValue(from: blueSlider)
+        setTextFrom(slider: redSlider)
+        setTextFrom(slider: greenSlider)
+        setTextFrom(slider: blueSlider)
         
         coloredView.layer.cornerRadius = 15
         
         setColor()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            super.touchesBegan(touches, with: event)
+            view.endEditing(true)
+        }
     
     @IBAction func doneButtonPressed() {
         delegate?.setBackground(
@@ -54,15 +67,7 @@ final class SettingsViewController: UIViewController {
     
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         setColor()
-        
-        switch sender {
-        case redSlider:
-            redLabel.text = getValue(from: redSlider)
-        case greenSlider:
-            greenLabel.text = getValue(from: greenSlider)
-        default:
-            blueLabel.text = getValue(from: blueSlider)
-        }
+        setTextFrom(slider: sender)
     }
     
     private func setColor() {
@@ -88,5 +93,78 @@ final class SettingsViewController: UIViewController {
         
         return (red, green, blue, alpha)
     }
+    
+    private func setTextFrom(slider: UISlider) {
+        switch slider {
+        case redSlider:
+            redLabel.text = getValue(from: slider)
+            redTextField.text = getValue(from: slider)
+        case greenSlider:
+            greenLabel.text = getValue(from: slider)
+            greenTextField.text = getValue(from: slider)
+        default:
+            blueLabel.text = getValue(from: slider)
+            blueTextField.text = getValue(from: slider)
+        }
+    }
+    
+    private func showAlert(withTitle title: String, andMessage message: String, sender: UITextField) {
+        let alert = UIAlertController(
+                    title: title,
+                    message: message,
+                    preferredStyle: .alert
+                )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+                    sender.text = "0.50"
+                }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
+
+// MARK: - UITextFieldDelegate
+extension SettingsViewController: UITextFieldDelegate {
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        guard let text = textField.text, let floatValue = Float(text) else {
+            showAlert(
+                withTitle: "Wrong format!",
+                andMessage: "Please enter correct value",
+                sender: textField
+            )
+            return false
+        }
+        if (0.0...1.0).contains(floatValue) {
+            return true
+        } else {
+            showAlert(
+                withTitle: "Wrong format!",
+                andMessage: "Please enter coorect value",
+                sender: textField
+            )
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        switch textField {
+        case redTextField:
+            guard let text = textField.text else { return }
+            redSlider.setValue(Float(text) ?? 0, animated: true)
+            sliderValueChanged(redSlider)
+        case greenTextField:
+            guard let text = textField.text else { return }
+            greenSlider.setValue(Float(text) ?? 0, animated: true)
+            sliderValueChanged(greenSlider)
+        default:
+            guard let text = textField.text else { return }
+            blueSlider.setValue(Float(text) ?? 0, animated: true)
+            sliderValueChanged(blueSlider)
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+}
